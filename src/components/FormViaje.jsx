68e75e0b5617f2d85a93f03fe889a2a6a45d502c
ivanboot <ivanma.garcia@hotmail.com>
2023-09-01 21:53:59 -0600

    Actualizando componente FormViaje

--- src/components/FormViaje.jsx
+++ src/components/FormViaje.jsx
@@ -31,77 +31,97 @@ const FormViaje = (props) => {
         console.log("Destino: " + destino);
         console.log("Cantidad de personas: " + cantPersona);
 
-         if (destino === "Panama") {
-            setCostoPaquete(197);
-            setPorcentaje(0.045); 
-            if(cantPersona==2){
-                setCostoPaquete(165);
-            }else if(cantPersona<=4){
-                setCostoPaquete(149);
-            }else if(cantPersona>=5){
-                setCostoPaquete(129);
+        if (cantPersona < 0) {
+            return;
+        }
+
+        var costo = 0;
+        var porcen = 0;
+        var impuest = 0;
+        var costoTotal = 0;
+
+        if (destino === "Panama") {
+            costo = 197;
+            porcen = 0.045;
+            if (cantPersona == 2) {
+                costo = 165;
+            } else if (cantPersona >= 3 && cantPersona <= 4) {
+                costo = 149;
+            } else if (cantPersona >= 5) {
+                costo = 129;
             }
-        }else if (destino === 'Mexico') {
-            setPorcentaje(0.06);
-            setCostoPaquete(485);
-            if(cantPersona==2){
-                setCostoPaquete(439);
-            }else if(cantPersona<=4){
-                setCostoPaquete(400);
-            }else if(cantPersona>=5){
-                setCostoPaquete(380);
+        } else if (destino === 'Mexico') {
+            porcen = 0.06;
+            costo = 485;
+            if (cantPersona == 2) {
+                costo = 439;
+            } else if (cantPersona >= 3 && cantPersona <= 4) {
+                costo = 400;
+            } else if (cantPersona >= 5) {
+                costo = 380;
             }
-        }else if (destino === 'Chile') {
-            setPorcentaje(0.07);
-            setCostoPaquete(198);
-            if(cantPersona==2){
-                setCostoPaquete(178);
-            }else if(cantPersona<=4){
-                setCostoPaquete(158);
-            }else if(cantPersona>=5){
-                setCostoPaquete(138);
+        } else if (destino === 'Chile') {
+            porcen = 0.07;
+            costo = 198;
+            if (cantPersona == 2) {
+                costo = 178;
+            } else if (cantPersona >= 3 && cantPersona <= 4) {
+                costo = 158;
+            } else if (cantPersona >= 5) {
+                costo = 138;
             }
-        }else if (destino === 'Peru') {
-            setPorcentaje(0.075);
-            setCostoPaquete(754);
-            if(cantPersona==2){
-                setCostoPaquete(699);
-            }else if(cantPersona<=4){
-                setCostoPaquete(649);
-            }else if(cantPersona>=5){
-                setCostoPaquete(629);
+        } else if (destino === 'Peru') {
+            porcen = 0.075;
+            costo = 754;
+            if (cantPersona == 2) {
+                costo = 699;
+            } else if (cantPersona >= 3 && cantPersona <= 4) {
+                costo = 649;
+            } else if (cantPersona >= 5) {
+                costo = 629;
             }
-        }else if (destino === 'Honduras') {
-            setPorcentaje(0.04);
-            setCostoPaquete(565);
-            if(cantPersona==2){
-                setCostoPaquete(499);
-            }else if(cantPersona<=4){
-                setCostoPaquete(469);
-            }else if(cantPersona>=5){
-                setCostoPaquete(449);
+        } else if (destino === 'Honduras') {
+            porcen = 0.04;
+            costo = 565;
+            if (cantPersona == 2) {
+                costo = 499;
+            } else if (cantPersona >= 3 && cantPersona <= 4) {
+                costo = 469;
+            } else if (cantPersona >= 5) {
+                costo = 449;
             }
         }
-        
-        setCostoPaquete((costoPaquete * cantPersona));
 
-        setImpuesto((costoPaquete * porcentaje) * cantPersona );
+        impuest = (costo * porcen) * cantPersona;
 
-        setTotal((costoPaquete + impuesto));
+        costo = (costo * cantPersona)
 
-        console.log("Porcentaje "+porcentaje);
-        console.log("Costo paquete "+costoPaquete);
-        console.log("Impuesto "+impuesto);
-        console.log("Total "+ total);
+        costoTotal = (costo + impuest);
+
+
+
+        setCostoPaquete(costo);
+        setPorcentaje(porcen);
+        setImpuesto(impuest);
+        setTotal(costoTotal);
+
+
+        console.log("Porcentaje " + porcentaje);
+        console.log("Costo paquete " + costoPaquete);
+        console.log("Impuesto " + impuesto);
+        console.log("Total " + total);
+
     }
 
     return (
         <>
-            <div className='container'>
+        <div className="row">
+            <div className='col-5'>
                 <h1>Planes de viaje</h1>
                 <div className="row justify-content-center">
                     <form onSubmit={e => e.preventDefault()}>
-                        <div className="col-6">
+                        <div className="">
+                        <label htmlFor="destino" className="form-label">Seleccione su destino turístico</label>
                             <select required={true} className="form-select" name='destino' value={destino} onChange={handleChangeDestino}>
                                 <option >Seleccione el una ciudad</option>
                                 <option value="Panama">Ciudad de Panamá</option>
@@ -111,16 +131,21 @@ const FormViaje = (props) => {
                                 <option value="Honduras">Rotan Honduras</option>
                             </select>
                         </div>
-                        <div className="col-6">
-                            <label htmlFor="ingredienteAdd" className="form-label">Ingredientes adicionales</label>
+                        <div className="">
+                            <label htmlFor="cantPersona" className="form-label">Ingredientes adicionales</label>
                             <input type="number" min={1} className="form-control" name='cantPersona' value={cantPersona} onChange={handleChangeCantidad} />
                         </div>
-                        <button className="btn btn-primary" onClick={handleClick} >Enviar</button>
+                        <button className="btn btn-primary col- mx-auto" onClick={handleClick} >Calcular costo</button>
 
                     </form>
 
                 </div>
             </div>
+            <div className="col-6">
+
+            </div>
+            </div>
+
         </>
     )
 }
